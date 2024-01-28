c03a0efa1e83dddf5c609354bb849b012a287ddb
Krzysztof Maziarz <krzysztof.maziarz@gmail.com>
2024-01-28 13:55:05 +0000

    Implement web app

--- fuel-painter/painter_web/src/constants.ts
+++ fuel-painter/painter_web/src/constants.ts
@@ -1,2 +1,6 @@
 
+<<<<<<< HEAD
 export const PainterContractID = "0xb09ddbbcd96f6ff24c800ef52327b50ddb7395405a54873358e129e2e7af583e"
+=======
+export const PainterContractID = "0xbbfcff7d64d3d3d2e15bfbef58f92014b0feb3cc5468905deac49fcb4b698488"
+>>>>>>> 151c430 (Implement web app)
